02efead1889630cd9c518b110bc15efcbabca127
olegus <olegus@64880915-06b1-486d-8577-4a13d8511655>
2008-11-11 15:58:41 +0000

    More examples on how to use AST tree and test services.
    git-svn-id: svn://dougdevel.org/oleg/projects/astvisualizer/trunk@139 64880915-06b1-486d-8577-4a13d8511655

--- astvis/services/dataflow.py
+++ astvis/services/dataflow.py
@@ -9,4 +9,12 @@ class DataflowService(core.Service):
 
     @action.Action('ast-reaching-definitions',"Reaching defs",targetClass=ast.ASTObject)
     def getReachingDefinitions(self, astNode, context=None):
-        print "reaching definitions"
+        
+        localEntities = [] 
+        
+        for decl in astNode.declarationBlock.statements:
+            localEntities.extend(decl.entities)
+
+        localNames = map(lambda e:e.name, localEntities)
+
+        return localNames
--- tests/test_dataflow.py
+++ tests/test_dataflow.py
@@ -2,16 +2,42 @@
 import tests.ast
 from astvis import core
 
-class DataflowTestCase(tests.ast.ASTTestCase):
+class FibonacciTestCase(tests.ast.ASTTestCase):
+    "Test case that tests dataflow on fibonacci algorithm."
+
+    FILENAME="fortran/fib.f90.xml"
 
     def setUp(self):
-        super(DataflowTestCase, self).setUp()
+        super(FibonacciTestCase, self).setUp()
 
         from astvis import services
         core.registerServices(services.dataflow)
         
         self.service = core.getService('DataflowService')
 
-    def testMe(self):
-        print self.service
+    def testFilesLoad(self):
+        "Test that file is loaded into AST model."
+        self.assertNotEqual(self.astModel, None)
+        self.assertEqual(len(self.astModel.files), 1)
+
+    def testFibonacci(self):
+        "Test that module has function named 'fibonacci'"
+        
+        fibModule = self.astModel.files[0].units[0]
+        fibonacciFunction = fibModule.subprograms[0]
+
+        self.assertEquals(fibonacciFunction.name.lower(), "fibonacci")
+
+    def testReachingDefinitions(self):
+        "Test reaching definition algorithm for fibonacci function"
+        
+        fibModule = self.astModel.files[0].units[0]
+        fibonacciFunction = fibModule.subprograms[0]
+
+        result = self.service.getReachingDefinitions(fibonacciFunction)
+        
+        self.assert_('n' in result)
+        self.assert_('r' in result)
+        
+        # more tests for the result follow here
 
